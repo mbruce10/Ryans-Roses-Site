de4eb6af41ba76fdbdb983263cfbb1da945f9e74
mbruce10 <megleigh2462@gmail.com>
2024-04-07 20:55:50 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Change color of instagram and facebook icons
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+
+// Add border around opening hours paragraph
+var openingHours = document.querySelector(".contact p");
+openingHours.style.border = "1px solid #d45bb9";
+
+// Add padding to opening hours paragraph
+openingHours.style.padding = "1em";
